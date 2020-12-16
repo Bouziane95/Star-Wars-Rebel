fa0e5025f9ca0b176516746e7a6d25f75176b816
Bouziane <bey.bouziane@live.fr>
2020-12-16 15:41:04 +0100

    getting all datas in back-end

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+const cors = require("cors");
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
@@ -21,6 +21,13 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:3001"],
+  })
+);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var axios = require("axios");
+const apiURL = "https://swapi.dev/api/";
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+function callApiWithEndpoint(endpoint) {
+  axios
+    .get(apiURL + endpoint)
+    .then((res) => {
+      console.log(res.data.results);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+callApiWithEndpoint("people");
+callApiWithEndpoint("starships");
+callApiWithEndpoint("planets");
+callApiWithEndpoint("vehicles");
+callApiWithEndpoint("films");
+callApiWithEndpoint("species");
 
 module.exports = router;
